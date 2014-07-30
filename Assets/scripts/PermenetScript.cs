3450bce008b0e247a10f755793f29abbdf15dfa8
Hyosung <hyosungk@sfu.ca>
2014-07-30 01:26:33 -0700

    Revert 0ac54fe..10be72a
    This rolls back to commit 0ac54fe7a1d3b09ed6df485eadbd534df30bdb89.

---
+++ Assets/scripts/PermenetScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PermenetScript : MonoBehaviour {
+
+	public int headNum;
+	public ArrayList items;
+	public ArrayList quests;
+	// Use this for initialization
+	void Start () {
+		items = new ArrayList ();
+		quests = new ArrayList ();
+	}
+	void Awake(){
+		DontDestroyOnLoad (this);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
